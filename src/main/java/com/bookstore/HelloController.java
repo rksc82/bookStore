1a3da0fb2ff2ae34d50cfe06f01b0bce725b7be4
rksc82 <rksc82@gmail.com>
2017-09-24 14:11:15 -0500

    first commit

---
+++ src/main/java/com/bookstore/HelloController.java
@@ -0,0 +1,15 @@
+package com.bookstore;
+
+import org.springframework.stereotype.*;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HelloController {
+
+    @RequestMapping("/")
+    @ResponseBody
+    String home() {
+        return "Hello World!";
+    }
+}
